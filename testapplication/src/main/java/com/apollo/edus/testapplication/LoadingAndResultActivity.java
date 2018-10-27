176797640963cd844024b1bcfdc003a20e546c5f
PandaPan <280853071@qq.com>
2018-10-27 11:02:12 +0800

    引用 通用的loading和result页面的组件

---
+++ testapplication/src/main/java/com/apollo/edus/testapplication/LoadingAndResultActivity.java
@@ -0,0 +1,13 @@
+package com.apollo.edus.testapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoadingAndResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_and_result);
+    }
+}
--- testapplication/src/main/java/com/apollo/edus/testapplication/MainActivity.java
+++ testapplication/src/main/java/com/apollo/edus/testapplication/MainActivity.java
@@ -1,13 +1,35 @@
 package com.apollo.edus.testapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mBtLoadAndResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
     }
+
+    private void initView() {
+        mBtLoadAndResult = findViewById(R.id.bt_loading_and_result);
+    }
+
+
+    private void initListener() {
+        mBtLoadAndResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoadingAndResultActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
