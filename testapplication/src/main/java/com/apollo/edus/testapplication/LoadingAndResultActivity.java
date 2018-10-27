29c1cc3614d4178673e5d0e3a7869499af39f3de
PandaPan <280853071@qq.com>
2018-10-27 12:17:53 +0800

    添加demo,完成加载组件的不同demo

--- testapplication/src/main/java/com/apollo/edus/testapplication/LoadingAndResultActivity.java
+++ testapplication/src/main/java/com/apollo/edus/testapplication/LoadingAndResultActivity.java
@@ -2,12 +2,64 @@ package com.apollo.edus.testapplication;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
+
+import com.apollo.edus.testapplication.fragment.LoadAndResultByBehaviorFragment;
+import com.apollo.edus.testapplication.fragment.LoadAndResultByMixFragment;
+import com.apollo.edus.testapplication.fragment.LoadAndResultByViewFragment;
 
 public class LoadingAndResultActivity extends AppCompatActivity {
+    private RadioGroup mRgSelector;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_and_result);
+        initView();
+        initListener();
+        initData();
+        //1. 采用behavior的方式 load
+        //2. 采用 view的方式 load
+        //3. 采用混合的方式 load
     }
+
+    private void initView() {
+        mRgSelector = findViewById(R.id.rg_selector);
+    }
+
+    private void initListener() {
+        mRgSelector.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i == R.id.bt_behavior){
+                    showFragment(LoadAndResultByBehaviorFragment.class);
+                }else if(i == R.id.bt_subview){
+                    showFragment(LoadAndResultByViewFragment.class);
+                }else if(i == R.id.bt_complex){
+                    showFragment(LoadAndResultByMixFragment.class);
+                }
+            }
+        });
+    }
+
+
+    private void initData() {
+        mRgSelector.check(R.id.bt_behavior);
+    }
+
+    private void showFragment(Class<? extends Fragment> fragmentClass){
+        if(fragmentClass == null){
+            return;
+        }
+        try {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragment_container, fragmentClass.newInstance()).commit();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
---
+++ testapplication/src/main/java/com/apollo/edus/testapplication/fragment/LoadAndResultByBehaviorFragment.java
@@ -0,0 +1,67 @@
+package com.apollo.edus.testapplication.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.apollo.edus.testapplication.R;
+import com.apollo.edus.uilibrary.widget.loadingandresult.LoadingAndResultContainer;
+
+public class LoadAndResultByBehaviorFragment extends Fragment {
+
+    private View mVLoading;
+    private View mVEmptyView;
+    private View mVErrorView;
+    private View mVResultView;
+    private LoadingAndResultContainer mLarcView;
+
+    private int index = 0;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_loading_and_result_by_behavior, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mLarcView = view.findViewById(R.id.larc_content);
+        mVLoading = mLarcView.getLoadingView();
+        mVEmptyView = mLarcView.getEmptyView();
+        mVErrorView = mLarcView.getErrorView();
+        mVErrorView.findViewById(R.id.tv_retry).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleLoadData();
+            }
+        });
+        mVResultView = mLarcView.getContentView();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        handleLoadData();
+
+    }
+
+    private void handleLoadData(){
+        mLarcView.showLoading();
+        mLarcView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                index++;
+                if(index % 2 == 1){//show error
+                    mLarcView.showError();
+                }else{//show result
+                    mLarcView.showCommonResult();
+                }
+            }
+        }, 400);
+    }
+}
