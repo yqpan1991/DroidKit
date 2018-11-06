6375e4df0806322ecf34206428387aa3a45f06eb
PandaPan <280853071@qq.com>
2018-11-07 07:45:52 +0800

    delete comments

--- uilibrary/src/main/java/com/apollo/edus/biz/aop/AopCallback.java
+++ uilibrary/src/main/java/com/apollo/edus/biz/aop/AopCallback.java
@@ -1,17 +1,3 @@
-/*
- * Project: DingTalk
- * 
- * File Created at 2018-11-02
- * 
- * Copyright 2015 Alibaba.com Corporation Limited.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Alibaba Company. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Alibaba.com.
- */
 package com.apollo.edus.biz.aop;
 
 import android.app.Activity;
